46041f0eb19d94c979bef86a3db2c99a2952a8c7
EstherWangui <esther.nganga@student.moringaschool.com>
2022-10-06 23:03:08 +0300

    done

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,14 @@
 // Code Keypad Component Here
+// Code Keypad Component Here
 
 function Keypad (){
-    return (
-        <div></div>
-    )
+  return (
+      <div>
+          <input type="password" onChange= {() => console.log("Entering password...")
+      }/>
+          
+      </div>
+  )
 }
 
 export default Keypad;
